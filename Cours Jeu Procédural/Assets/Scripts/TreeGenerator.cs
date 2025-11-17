9fe7ca00de2a5f487fe130f5f7b5a072c1c27ebe
Lynn <jacquelinehuynh@outlook.fr>
2025-11-17 11:14:58 +0100

    Adding PoissonDiscSampling in project

--- Cours Jeu Procédural/Assets/Scripts/TreeGenerator.cs
+++ Cours Jeu Procédural/Assets/Scripts/TreeGenerator.cs
@@ -2,30 +2,56 @@ using UnityEngine;
 
 public class TreeGenerator : MonoBehaviour
 {
+    [ExecuteAlways]
     [SerializeField] private int numberMax;
+    [SerializeField] private int _seed;
 
     public Vector3 spawnAreaMin;
     public Vector3 spawnAreaMax;
-    public GameObject[] trees; 
+    public GameObject[] trees;
 
-    void Start()
+    public GameObject treeGenerator;
+
+    private bool _needsUpdate;
+
+    private void OnValidate()
     {
-        for (int i = 0; i < numberMax; i++)
+        _needsUpdate = true;
+    }
+
+    private void Update()
+    {
+        if (_needsUpdate)
         {
             SpawnRandom();
+            _needsUpdate = false;
         }
     }
 
-    void SpawnRandom()
+    private void SpawnRandom()
     {
-        float randomX = Random.Range(spawnAreaMin.x, spawnAreaMax.x);
-        float randomZ = Random.Range(spawnAreaMin.z, spawnAreaMax.z);
+        
+        Random.InitState(_seed); //Number of Seed (1 seed = 1 random map = same map if same seed)
 
-        Vector3 pos = new Vector3(randomX, 0f, randomZ);
+        // Clear unwanted trees went we reduce the amount of trees
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            Transform child = transform.GetChild(i);
+            DestroyImmediate(child.gameObject);
+        }
 
-        // random tree in list
-        GameObject prefab = trees[Random.Range(0, trees.Length)];
 
-        Instantiate(prefab, pos, Quaternion.identity);
-    }
+        for (int i = 0; i < numberMax; i++)
+        {
+            float randomX = Random.Range(spawnAreaMin.x, spawnAreaMax.x);
+            float randomZ = Random.Range(spawnAreaMin.z, spawnAreaMax.z);
+
+            Vector3 pos = new Vector3(randomX, 0f, randomZ);
+
+            // random tree in list
+            GameObject prefab = trees[Random.Range(0, trees.Length)];
+
+            Instantiate(prefab, pos, Quaternion.identity, treeGenerator.transform);
+        }
+    }  
 }
